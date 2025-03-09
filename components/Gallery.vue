13bc743540a775ee088d9e562d687aca1d6d50de
Axel Ganter <mail@axelganter.de>
2025-03-09 20:47:05 +0100

    count dogs

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,52 +1,72 @@
 <template>
-    <div class="gallery">
-      <div 
-        v-for="img in images" 
-        :key="img" 
-        class="img-container"
-        @click="handleClick(img)"
-      >
-        <img :src="img" alt="Hund oder Katze" />
+    <div>
+      <div class="gallery">
+        <div 
+          v-for="img in images" 
+          :key="img" 
+          class="img-container"
+          @click="handleClick(img)"
+        >
+          <img :src="img" alt="Hund oder Katze" />
+        </div>
       </div>
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue'
-  
-  function generateGallery() {
-    const countDogs = 1
-    const countCats = 15
-    const randomDogs = Array.from({ length: countDogs }, () => Math.floor(Math.random() * 100) + 1)
-    const randomCats = Array.from({ length: countCats }, () => Math.floor(Math.random() * 100) + 1)
-    const pathToCatImages = '/PetImages/Cat/'
-    const pathToDogImages = '/PetImages/Dog/'
-    const catImages = randomCats.map(num => `${pathToCatImages}${num}.jpg`)
-    const dogImages = randomDogs.map(num => `${pathToDogImages}${num}.jpg`)
-    const allImages = [...catImages, ...dogImages]
-    return [...allImages].sort(() => Math.random() - 0.5).slice(0, 16)
-  }
-  
-  const images = ref(generateGallery())
-  
-  function refreshGallery() {
-    images.value = generateGallery()
-  }
-  
-  function removeImage(img) {
-    images.value = images.value.filter(i => i !== img)
-  }
-  
-  function handleClick(img) {
-    if (img.includes('/Dog/')) {
-        refreshGallery()
-    } else if (img.includes('/Cat/')) {
-        removeImage(img)
-    }
+import { ref, watch } from 'vue'
+const emit = defineEmits(['update-dog-counter'])
+
+function getUniqueRandomNumbers(count, max) {
+  const numbers = Array.from({ length: max }, (_, i) => i + 1)
+  return numbers.sort(() => Math.random() - 0.5).slice(0, count)
 }
-  </script>
+
+function generateGallery() {
+  const countDogs = 1
+  const countCats = 15
+  const randomDogs = getUniqueRandomNumbers(countDogs, 100)
+  const randomCats = getUniqueRandomNumbers(countCats, 100)
+  const pathToCatImages = '/PetImages/Cat/'
+  const pathToDogImages = '/PetImages/Dog/'
+  const catImages = randomCats.map(num => `${pathToCatImages}${num}.jpg`)
+  const dogImages = randomDogs.map(num => `${pathToDogImages}${num}.jpg`)
+  const allImages = [...catImages, ...dogImages]
+  return allImages.sort(() => Math.random() - 0.5).slice(0, 16)
+}
+
+const images = ref(generateGallery())
+
+function refreshGallery() {
+  images.value = generateGallery()
+}
+
+function removeImage(img) {
+  images.value = images.value.filter(i => i !== img)
+}
+
+const dogCounter = ref(0)
+
+watch(dogCounter, newVal => {
+  emit('update-dog-counter', newVal)
+})
+
+function handleClick(img) {
+  if (img.includes('/Dog/')) {
+    dogCounter.value++
+    refreshGallery()
+  } else if (img.includes('/Cat/')) {
+    removeImage(img)
+  }
+}
+</script>
+
   
   <style scoped>
+  .counter {
+    font-size: 1.2rem;
+    margin-bottom: 10px;
+  }
   .gallery {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-    <Gallery />
-  </template>
-  
-  <script setup>
-  import Gallery from '~/components/Gallery.vue'
-  </script>
-  
+  <div>
+    <h1>Auf den Hund gekommen</h1>
+    <Gallery @update-dog-counter="dogCounter = $event" />
+    <div class="counter">Hunde gefunden: {{ dogCounter }} </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Gallery from '~/components/Gallery.vue'
+
+const dogCounter = ref(0)
+</script>
+
+<style scoped>
+.counter {
+  margin-top: 40px;
+}
+</style>
