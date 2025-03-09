8dc7136976792b0a054e09d4d02d58653ec37e06
Axel Ganter <mail@axelganter.de>
2025-03-09 20:27:51 +0100

    init

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: monospace;
+  font-size: 1.5rem;
+}
+.app-container {
+  background-color: black;
+  color: green;
+  min-height: 100vh;
+  padding: 20px;
+}
+</style>
---
+++ components/Gallery.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="gallery">
+      <div v-for="img in images" :key="img" class="img-container">
+        <img :src="img" alt="Hund oder Katze" />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { computed } from 'vue'
+  
+  const countDogs = 1
+  const countCats = 15
+  const randomDogs = Array.from({ length: countDogs }, () => Math.floor(Math.random() * 100) + 1)
+  const randomCats = Array.from({ length: countCats }, () => Math.floor(Math.random() * 100) + 1)
+  const pathToCatImages = '/PetImages/Cat/'
+  const pathToDogImages = '/PetImages/Dog/'
+  
+  const catImages = randomCats.map(num => `${pathToCatImages}${num}.jpg`)
+  const dogImages = randomDogs.map(num => `${pathToDogImages}${num}.jpg`)
+  const allImages = [...catImages, ...dogImages]
+  
+  const images = computed(() =>
+    [...allImages].sort(() => Math.random() - 0.5).slice(0, 16)
+  )
+  </script>
+  
+  <style scoped>
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .img-container {
+    width: 25%;
+    padding: 5px;
+  }
+  .img-container img {
+    width: 100%;
+    height: auto;
+  }
+  </style>
+  
