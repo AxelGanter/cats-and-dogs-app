a8012bc25595b5cc154736e823c80f9930f5fe8a
Axel Ganter <mail@axelganter.de>
2025-03-09 20:34:42 +0100

    remove cats

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,48 +1,71 @@
 <template>
     <div class="gallery">
-        <div v-for="img in images" :key="img" class="img-container">
-            <img :src="img" alt="Hund oder Katze" />
-        </div>
+      <div 
+        v-for="img in images" 
+        :key="img" 
+        class="img-container"
+        @click="handleClick(img)"
+      >
+        <img :src="img" alt="Hund oder Katze" />
+      </div>
     </div>
   </template>
   
   <script setup>
-  import { computed } from 'vue'
+  import { ref } from 'vue'
   
-  const countDogs = 1
-  const countCats = 15
-  const randomDogs = Array.from({ length: countDogs }, () => Math.floor(Math.random() * 100) + 1)
-  const randomCats = Array.from({ length: countCats }, () => Math.floor(Math.random() * 100) + 1)
-  const pathToCatImages = '/PetImages/Cat/'
-  const pathToDogImages = '/PetImages/Dog/'
+  function generateGallery() {
+    const countDogs = 1
+    const countCats = 15
+    const randomDogs = Array.from({ length: countDogs }, () => Math.floor(Math.random() * 100) + 1)
+    const randomCats = Array.from({ length: countCats }, () => Math.floor(Math.random() * 100) + 1)
+    const pathToCatImages = '/PetImages/Cat/'
+    const pathToDogImages = '/PetImages/Dog/'
+    const catImages = randomCats.map(num => `${pathToCatImages}${num}.jpg`)
+    const dogImages = randomDogs.map(num => `${pathToDogImages}${num}.jpg`)
+    const allImages = [...catImages, ...dogImages]
+    return [...allImages].sort(() => Math.random() - 0.5).slice(0, 16)
+  }
   
-  const catImages = randomCats.map(num => `${pathToCatImages}${num}.jpg`)
-  const dogImages = randomDogs.map(num => `${pathToDogImages}${num}.jpg`)
-  const allImages = [...catImages, ...dogImages]
+  const images = ref(generateGallery())
   
-  const images = computed(() =>
-    [...allImages].sort(() => Math.random() - 0.5).slice(0, 16)
-  )
+  function refreshGallery() {
+    images.value = generateGallery()
+  }
+  
+  function removeImage(img) {
+    images.value = images.value.filter(i => i !== img)
+  }
+  
+  function handleClick(img) {
+    if (img.includes('/Dog/')) {
+        refreshGallery()
+    } else if (img.includes('/Cat/')) {
+        removeImage(img)
+    }
+}
   </script>
   
-<style scoped>
-.gallery {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 5px;
-}
-.img-container {
-  position: relative;
-  width: 100%;
-  padding-top: 100%; /* Erzeugt ein Quadrat */
-  overflow: hidden;
-}
-.img-container img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* Zoomt das Bild so, dass es den Container füllt */
-}
-</style>
+  <style scoped>
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 5px;
+  }
+  .img-container {
+    position: relative;
+    width: 100%;
+    padding-top: 100%;
+    overflow: hidden;
+    cursor: pointer;
+  }
+  .img-container img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  </style>
+  
