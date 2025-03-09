73a91818ab9e9dd540a24b8e0b72d5b27edb53c8
Axel Ganter <mail@axelganter.de>
2025-03-09 23:37:09 +0100

    chore: reorganize code

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -23,55 +23,10 @@
   </template>
   
   <script setup>
-
   import { ref, onMounted, watch } from 'vue'
+  import { generateGallery } from '../composables/gallery.js'
   const emit = defineEmits(['update-dog-counter'])
-  
-  function getUniqueRandomNumbers(count, max, exclude = []) {
-    const numbers = new Set();
-    while (numbers.size < count) {
-      const num = Math.floor(Math.random() * max) + 1;
-      if (!exclude.includes(num)) {
-        numbers.add(num);
-      }
-    }
-    return Array.from(numbers);
-  }
-  
-  function generateGallery() {
-    const countDogs = 1
-    const countCats = 15
-    // Stelle sicher, dass die Hund-ID nicht in den Katzen-IDs vorkommt:
-    const randomDogs = getUniqueRandomNumbers(countDogs, 10000)
-    const randomCats = getUniqueRandomNumbers(countCats, 10000, randomDogs)
     
-    const pathToCatImages = '/PetImages/Cat/'
-    const pathToDogImages = '/PetImages/Dog/'
-    
-    const catPets = randomCats.map(num => ({
-      type: 'cat',
-      id: 'cat'+num,
-      url: `${pathToCatImages}${num}.jpg`,
-      img: `${pathToCatImages}previews/${num}.jpg`,
-      
-    }))
-    
-    const dogPets = randomDogs.map(num => ({
-      type: 'dog',
-      id: 'dog'+num,
-      url: `${pathToDogImages}${num}.jpg`,
-      img: `${pathToDogImages}previews/${num}.jpg`,
-      
-    }))
-    
-    const pets = [...catPets, ...dogPets]
-    const petsRandom = pets.sort(() => Math.random() - 0.5)
-
-    console.log(pets)
-    
-    return petsRandom
-  }
-  
   const pets = ref(generateGallery())  
   const dogCounter = ref(0)
   
