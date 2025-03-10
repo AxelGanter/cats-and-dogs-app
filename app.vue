23caf79bf1850d4d5b2a0cc93a27cec48a8eb40d
Axel Ganter <mail@axelganter.de>
2025-03-10 01:34:40 +0100

    feat: vercel insights

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <template>
+  
   <div class="app-container">
     <NuxtPage />
+    <SpeedInsights />
   </div>
 </template>
 
 <script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
 
 <style>
